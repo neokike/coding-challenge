cc80c29e973bd6ec7bdccbf8a35613a6fc0cca60
Pedro Gorrin <pedro.gorrin@ngncloud.com>
2016-06-10 15:12:28 -0430

    Configuración de pruebas unitarias

---
+++ app/Cubo.php
@@ -0,0 +1,21 @@
+<?php
+namespace App;
+
+class Cubo
+{
+
+    public function inicializar()
+    {
+
+    }
+
+    public function update($x, $y, $z, $valor)
+    {
+
+    }
+
+    public function query($x1, $y1, $z1, $x2, $y2, $z2)
+    {
+        
+    }
+}
---
+++ tests/CuboTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CuboTest extends TestCase
+{
+    /**
+     * Prueba la inicializacion en 0 del cubo.
+     *
+     * @return void
+     */
+    public function testInicializar()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Prueba el proceso de update.
+     *
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Prueba el proceso de query.
+     *
+     * @return void
+     */
+    public function testQuery()
+    {
+        $this->assertTrue(true);
+    }
+
+}
